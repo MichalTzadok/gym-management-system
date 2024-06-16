7521a67f0c8ed94757f61224d3115a1d97ea6ed7
MichalTzadok <michal9741003@gmail.com>
2024-06-17 00:45:16 +0300

    add adminOnly middleware to protected functions

--- node-server/app.ts
+++ node-server/app.ts
@@ -4,6 +4,10 @@ import connectDB from './config/db';
 import config from './config/config';
 import userRoutes from './routers/user.router';
 import swaggerMiddleware from './middleware/swagger.middleware'; 
+import serviceRoutes from './routers/service.router';
+import businessRoutes from './routers/business.router';
+import meetingRoutes from './routers/meeting.router';
+import authRoutes from './routers/auth.router'
 
 const app = express();
 const port = config.server.port;
@@ -14,7 +18,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 swaggerMiddleware(app);
+app.use('/', authRoutes);
 app.use('/users', userRoutes);
+app.use('/services', serviceRoutes);
+app.use('/business', businessRoutes);
+app.use('/meetings', meetingRoutes);
+
 app.listen(port, () => {
   console.log(` app listening on port ${port}`);
 });
