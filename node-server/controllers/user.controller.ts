d2a533bf408b9709f39e5dd6a65f581e11aa8ae6
MichalTzadok <michal9741003@gmail.com>
2024-06-16 23:30:55 +0300

    Separation of the signup and signin functions to a separate router

--- node-server/controllers/user.controller.ts
+++ node-server/controllers/user.controller.ts
@@ -1,28 +1,7 @@
 import { Response, Request } from 'express';
-import { getUser, getUsers, signIn, signUp} from '../services/user.service';
-import { CustomError } from '../types/custonError';
+import { getUser, getUsers} from '../services/user.service';
+import { CustomError } from '../types/customError';
 
-
-export const post_signup = async (req: Request, res: Response) => {
-    try {
-        const result = await signUp(req);
-        console.log(result);
-        return res.status(201).send('User signed up successfully');
-    } catch (error) {
-        console.error('Error during signup: ', error);
-        return res.status((error instanceof CustomError && error.code) || 500).json({message: error instanceof CustomError && error.message ||  'An error occurred during signup'});
-    }
-};
-
-export const post_signin = async (req: Request, res: Response) => {
-    try {
-        const  {user,token} = await signIn(req);
-        return res.status(201).json({ message: 'User signed in successfully', user: user, token: token });
-    } catch (error) {
-        console.error('Error during signin: ', error);
-        return res.status((error instanceof CustomError && error.code) || 500).json({message: error instanceof CustomError && error.message ||  'An error occurred during signin'});
-    }
-};
 
 export const get_user = async (req: Request, res: Response) => {
     try {
--- node-server/services/user.service.ts
+++ node-server/services/user.service.ts
@@ -1,49 +1,7 @@
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
 import { Request } from 'express';
 import { UserModel } from '../models/user.model';
-import config from '../config/config';
-import { validateEmail } from '../utils/emailValidator'; 
-import { CustomError } from '../types/custonError';
+import { CustomError } from '../types/customError';
 
-export const signUp = async (req: Request) => {
-  const { username, email, password, role } = req.body;
-  if (!username || !email || !password || !role)
-      throw new CustomError('Missing required fields',400);
-  // const isEmailValid = await validateEmail(email);
-  // if (!isEmailValid)
-  //     throw new CustomError('Invalid email address',422);
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const newUser = new UserModel({
-      username: username,
-      password: hashedPassword,
-      email: email,
-      role: role
-  });
-  try {
-      await newUser.save();
-  } catch (error) {
-      console.error('Error saving user:', error);
-      throw new CustomError('Failed to save user',500);
-  }
-  return newUser;
-};
-export const signIn = async (req: Request) => {
-  try {
-    const { email, password } = req.body;
-    const user = await UserModel.findOne({ email: email });       
-    if (!user)
-        throw new CustomError('User not found',404);
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch)
-        throw new CustomError('Invalid credentials',401);
-    const token = jwt.sign({ id: user._id, role: user.role }, config.jwtSecret as string, { expiresIn: '1h' });
-    return { user, token };
-} catch (error) {
-    console.error('Error signing in:', error);
-    throw error;
-}
-};
 
 
 export const getUsers = async () => {
